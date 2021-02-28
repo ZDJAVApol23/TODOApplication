45e8bfce53b3d743f207632f760cd7d0c531cd9c
Marcin Szupke <marcin.szupke.sda@gmail.com>
2021-02-28 10:59:26 +0100

    zamykanie zadań z poziomu listy zadań

--- src/main/resources/static/js/todo.js
+++ src/main/resources/static/js/todo.js
@@ -13,3 +13,19 @@ $('#close-todo-button').click(function () {
         }
     });
 });
+
+$('.close-todo-from-list').click(function () {
+    var todoId = $(this).parents('tr').find('input[type="hidden"]').val();
+
+    $.ajax({
+        url: '/todo/' + todoId,
+        type: 'PUT',
+        async: false,
+        success: function (result) {
+            window.location.replace("/todo");
+        },
+        error: function (error) {
+            alert('Wystąpił nieoczekiwany błąd')
+        }
+    });
+})
